5e55a82635f7389708f026d88a9c4fc8e1b7589e
Brian Marinai <brianmarinai.bm@gmail.com>
2020-06-30 15:44:42 +0200

    esercizio in corso

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Il computer deve generare 16 numeri casuali tra 1 e 100
+
+
+
+
+for (var i = 0; i < 5; i++) {
+  Math.floor(Math.random() * 100) + 1;
+  console.log(i);
+}
+// chiedere all'utente di inserire un numero alla volta (16-100) volte, sempre tra 1 e 100
